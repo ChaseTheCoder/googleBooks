b7181cd19e1d1b75306aa2108c72c7d2565b2827
ChaseTheCoder <chasefsheaff@gmail.com>
2021-06-14 20:19:54 -0500

    feat: add prompt using inquirer package

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const { prompt } = require('inquirer');
+const books = require('google-books-search');
+
+const askUser = () => {
+  return prompt([
+    {
+      type: 'list',
+      name: 'initialChoices',
+      message: 'Google Book Search: Select a Choice',
+      choices: ['Search books', 'View Reading List', 'Done!']
+    }
+  ])
+};
+
+const start = () => {
+  console.log('','\n','\n','\n',"Welcome to Google Book Search",'\n');
+  askUser().then(answer => {
+    if(answer.initialChoices === 'Search books') {
+      console.log("searching .........");
+    }
+  })
+}
+
+start();
